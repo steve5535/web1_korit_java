f2973f110b06418682731adb448663c51eeb92b4
steve5535 <steve5535@naver.com>
2026-04-17 21:55:11 +0900

    오타수정

--- src/main/java/_01_Print/TypeCasting.java
+++ src/main/java/_01_Print/TypeCasting.java
@@ -5,14 +5,14 @@ public class TypeCasting {
         // (자료)형변환: 데이터 타입을 다른 타입으로 변경
 
         // 큰 -> 작은: 넘칠 수 있음 -> 데이터유실 가능성(명시적 형변환)
-        // 작은 -> 큰: 데이터유실 가능성 x(자동 형변환
+        // 작은 -> 큰: 데이터유실 가능성 x(자동 형변환)
 
         // 작은 -> 큰
         int intNum = 10; // 4바이트
-        long lonNum; // 8바이트
+        long longNum; // 8바이트
         double doubleNum; // 8바이트
 
-        lonNum = 10; // 10L로 자동변환됨
+        longNum = 10; // 10L로 자동변환됨
         doubleNum = intNum; // 10.0으로 자동변환됨
 
         // 큰 -> 작은(오버플로우)
